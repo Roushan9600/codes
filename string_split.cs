32df744dbe4be40f420e79b23a164d231eefc4bc
Roushan <Rprasad9600@gmail.com>
2021-07-15 10:11:45 +0530

    string_split

---
+++ string_split.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "my name is roushan kumar";
+            string [] str1 = str.Split(' ');
+            foreach (var p in str1)
+            {
+                Console.WriteLine(p);
+            }
+            Console.ReadLine();
+        }
+    }
+}
