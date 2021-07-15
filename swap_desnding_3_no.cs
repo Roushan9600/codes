0145a63f154dd73aece2dfbee720fcbf29791e66
Roushan <Rprasad9600@gmail.com>
2021-07-15 10:37:16 +0530

    3n0_sort

---
+++ swap_desnding_3_no.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swap_3_no
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c,max,mid,min;
+            Console.WriteLine("enter 3 integer no");
+            a=Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("enter 3 integer no");
+            b=Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("enter 3 integer no");
+            c=Convert.ToInt32(Console.ReadLine());
+
+            if (a > b && a > c)
+                max = a;
+            {
+                if (b > c)
+                    max = b;
+                
+                else
+                        max = c;  
+            }
+            
+
+            if (a < b && a < c)
+                min = a;
+            {
+                if (b < c)
+                    min = b;
+                else
+                    min = c;
+            }
+
+            {
+            mid = a + b + c - max - min;
+            }
+
+            Console.WriteLine("a="+max);
+            Console.WriteLine("b="+mid);
+            Console.WriteLine("c="+min);
+            Console.ReadLine();
+        }
+    }
+}
