64088003c4fc2b1aa5c19b53d659c5b19431ea03
Roushan9600 <87384224+Roushan9600@users.noreply.github.com>
2021-07-17 13:53:48 +0530

    squre_pattern

---
+++ squre.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=1;i<=5;i++)
+            {
+                for(int j=1;j<=5;j++)
+                {
+                    if(i<=5 &&j==1||i==1&&j<=5||j==5||i==5||i==j||j==6-i)
+                    {
+                    Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("  ") ;
+                    }
+                }
+                
+                Console.WriteLine();
+            }
+           
+            Console.ReadLine();
+        }
+    }
+}
